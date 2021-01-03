a2c217b4a17cda22dadff13218501f1e379211ce
subhadeep <subhag002@gmail.com>
2021-01-03 17:36:24 +0530

    Another Project

---
+++ script.js
@@ -0,0 +1,25 @@
+function updateTime(){
+    const hourEL=document.querySelector("#hour");
+    const minuteEL=document.querySelector("#minute");
+    const secondEL=document.querySelector("#second");
+    let d=new Date();
+    let hours=d.getHours();
+    let minutes=d.getMinutes();
+    let seconds=d.getSeconds();
+    if(hours>=0 && hours<=9) hours="0"+hours;
+    if(minutes>=0 && minutes<=9) minutes="0"+minutes;
+    if(seconds>=0 && seconds<=9) seconds="0"+seconds;
+    hourEL.innerHTML=hours;
+    minuteEL.innerHTML=minutes;
+    secondEL.innerHTML=seconds;
+
+     const dateEL=document.querySelector("#date");
+     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+     dateEL.innerHTML=d.toLocaleDateString(undefined,options);
+     
+     
+}
+
+window.onload=function(){
+    setInterval(updateTime,1000);
+}
